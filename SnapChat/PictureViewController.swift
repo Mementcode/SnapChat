49d4422fcdc4a23017877e9698810ee09cc9bb38
callum brennan <callum.brennan@hotmail.co.uk>
2017-04-17 14:06:58 +0100

    working -  snapchat Clone - lesson 10

--- SnapChat/PictureViewController.swift
+++ SnapChat/PictureViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import Firebase // INCLUDE ME!!!
 
-class PictureViewController: UIViewController {
+// ALL 3 MUST BE INCLUDED
+class PictureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
   
     @IBOutlet weak var nextButton: UIButton!
@@ -17,18 +19,66 @@ class PictureViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     
+     var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       imagePicker.delegate = self  // INCLUDE ME!
+
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        imageView.image = image
+        
+        imageView.backgroundColor = UIColor.clear
+        imagePicker.dismiss(animated: true, completion: nil)
+        
     }
 
     @IBAction func CameraTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .savedPhotosAlbum // recives images from photo album
+        imagePicker.allowsEditing = false // doesnt allow editing in app
+        
+      present(imagePicker, animated: true, completion: nil)
+        
         
     }
   
     @IBAction func nextTapped(_ sender: Any) {
         
+        nextButton.isEnabled = false // greys out button after it is tapped
+        
+        let imagesFolder = FIRStorage.storage().reference().child("image") // looks to the correct folder in firebase, storage, and then opens up folder "image"
+        
+        let imageData = UIImageJPEGRepresentation(imageView.image!, 0.1)! // converts the image to a JPEG with a compresion of 0.1 to speed up the app, and assins to imageData
+        
+        
+        
+        
+        // uploads imageData to the folder "image" on fire base and returns any errors, if there are no errors then the segue is moved to the "selectUserSegue"
+        // adds the NSUUID name with a .jpg at the end
+        
+        imagesFolder.child("\(String(describing: NSUUID().uuidString)).jpg").put(imageData, metadata: nil, completion: {( metadata, error) in
+            print("We tried to Upload")
+            
+            if error != nil {
+                
+                print ("we had an error:\(String(describing: error))")
+            } else {
+                
+                print(metadata!.downloadURLs!)
+                self.performSegue(withIdentifier: "selectUserSegue", sender: nil)
+            }
+            
+        })
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+ 
+}
 }
---
+++ SnapChat/SelectUserViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SelectUserViewController.swift
+//  SnapChat
+//
+//  Created by callum brennan on 17/04/2017.
+//  Copyright © 2017 callum brennan Mementcode. All rights reserved.
+//
+
+import UIKit
+
+class SelectUserViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
